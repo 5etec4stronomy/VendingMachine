2d5960c9f6eb47215431020b3bdf521d71530b09
Tim Drinkhill <tim@dogchunx.co.uk>
2020-03-03 09:20:23 +0000

    tested for invalid coin

--- VendingMachine.Tests/CoinValidatorTests.cs
+++ VendingMachine.Tests/CoinValidatorTests.cs
@@ -25,5 +25,15 @@ namespace VendingMachine.Tests
             Assert.IsTrue(_sut.MatchedCoinResult.ValidCoin);
             Assert.AreEqual(CoinType.Nickel, _sut.MatchedCoinResult.Coin.CoinType);
         }
+
+        [Test]
+        public void ValidateCoin_InvalidCoin_ReturnsValidCoinResult()
+        {
+            var testCoin = new Coin { Diameter = 25m, Weight = 6 };
+
+            _sut.ValidateCoin(testCoin);
+
+            Assert.IsFalse(_sut.MatchedCoinResult.ValidCoin);
+        }
     }
 }
