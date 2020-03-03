5cea1bf74d7df642151a0e069d4468a6d27cda2c
Tim Drinkhill <tim@dogchunx.co.uk>
2020-03-03 08:35:49 +0000

    added failing Processor default message test

---
+++ VendingMachine.Tests/ProcessorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Tests
+{
+    [TestFixture]
+    public class ProcessorTests
+    {
+        [Test]
+        public void DefaultMessage_SetMessage_ReturnsCorrectMessage()
+        {
+            IProcessor sut = new Processor();
+        }
+    }
+}
