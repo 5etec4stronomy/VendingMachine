31a6cf6f7abaaf3f307fa39c9cb5248cffe4b54f
Tim Drinkhill <tim@dogchunx.co.uk>
2020-03-03 08:44:31 +0000

    added test setup

--- VendingMachine.Tests/ProcessorTests.cs
+++ VendingMachine.Tests/ProcessorTests.cs
@@ -8,13 +8,20 @@ namespace VendingMachine.Tests
     [TestFixture]
     public class ProcessorTests
     {
+        private IProcessor _sut;
+        private IDisplay _display;
+
+        [SetUp]
+        public void Setup()
+        {
+            _display = new Display();
+            _sut = new Processor(_display);
+        }
+
         [Test]
         public void DefaultMessage_SetMessage_ReturnsCorrectMessage()
         {
-            IDisplay display = new Display();
-            IProcessor sut = new Processor(display);
-
-            Assert.AreEqual("INSERT COIN", display.Message);
+            Assert.AreEqual("INSERT COIN", _display.Message);
         }
     }
 }
