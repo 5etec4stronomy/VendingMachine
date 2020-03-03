18b2bb0d8d0aa95eb44cf26cb8a8d9173404eaf6
Tim Drinkhill <tim@dogchunx.co.uk>
2020-03-03 09:43:00 +0000

    added product object

---
+++ VendingMachine/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine
+{
+    public class Product
+    {
+        public ProductType ProductType { get; set; }
+        public decimal SellPrice { get; set; }
+        public int StockLevel { get; set; }
+    }
+
+    public enum ProductType
+    {
+        Cola,
+        Chips,
+        Candy
+    }
+}
