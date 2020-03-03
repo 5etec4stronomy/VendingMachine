1194980ce5c305b460d8be7180e8ff9b5f88fc85
Tim Drinkhill <tim@dogchunx.co.uk>
2020-03-03 08:28:35 +0000

    fixed failing test and fleshed out method

--- VendingMachine/Display.cs
+++ VendingMachine/Display.cs
@@ -6,6 +6,10 @@ namespace VendingMachine
     {
         public string Message { get; set; }
 
+        /// <summary>
+        /// Displays the supplied message -- currently this just sets a string, but this interface could be used to drive an external display etc
+        /// </summary>
+        /// <param name="message">Message to display</param>
         public void SetMessage(string message)
         {
             Message = message;
--- VendingMachine/IDisplay.cs
+++ VendingMachine/IDisplay.cs
@@ -3,6 +3,7 @@
     public interface IDisplay
     {
         string Message { get; }
+
         void SetMessage(string message);
     }
 }
